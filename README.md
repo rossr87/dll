f2b1ce824ebe74fd889157467455a8f5a146397b
Ross R <singualrity.imminent@gmail.com>
2021-01-06 11:54:32 +0000

    Removed surplus print statements and comments to acommodate TDD and re-orientation with C.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dll
 Doubly Linked List Library
+
+Phase 1:
+
+Implement simple dll
+
+Phase 2:
+
+Add a tail. See if certain operations are simplified or faster by design/performance.
--- dll.h
+++ dll.h
@@ -1,4 +1,23 @@
+typedef struct dll_node_ {
+    void *data;
+    struct dll_node_ *left;
+    struct dll_node_ *right;
+} dll_node_t;
+
+typedef struct dll_ {
+    dll_node_t *head;
+} dll_t;
+
 /*
- * variable declarations and function definitions for
- * doubly linked list.
+ * allocate a new list on the heap.
+ * this is merely a pointer to a dll_node_t (dll node)
+ * then return the address of this pointer.
  */
+dll_t *get_new_dll();
+
+/*
+ * append data to list.
+ * inputs: pointer to the list: the list is on the heap and we need to know where it is
+ */
+int append_item(dll_t *list, void *app_data);
+int prepend_item(dll_t *list, void *app_data);
