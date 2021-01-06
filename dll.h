8efe56b59618809c374ec4342854ceb8dd06bdd1
Ross R <singualrity.imminent@gmail.com>
2021-01-06 11:59:45 +0000

    Removed babbling comments.

--- dll.h
+++ dll.h
@@ -8,16 +8,6 @@ typedef struct dll_ {
     dll_node_t *head;
 } dll_t;
 
-/*
- * allocate a new list on the heap.
- * this is merely a pointer to a dll_node_t (dll node)
- * then return the address of this pointer.
- */
 dll_t *get_new_dll();
-
-/*
- * append data to list.
- * inputs: pointer to the list: the list is on the heap and we need to know where it is
- */
 int append_item(dll_t *list, void *app_data);
 int prepend_item(dll_t *list, void *app_data);
