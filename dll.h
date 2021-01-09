9ebc565a376045022b31586243a2b8dc0589bc76
Ross R <singualrity.imminent@gmail.com>
2021-01-09 11:51:15 +0000

    Added function prototypes to dll.h

--- dll.h
+++ dll.h
@@ -14,5 +14,7 @@ typedef struct dll_ {
 dll_t *get_new_dll();
 int append_item(dll_t *list, void *app_data);
 int prepend_item(dll_t *list, void *app_data);
-
+int remove_data_from_dll_by_data_ptr(dll_t list*, void *app_data);	/* return 0 on success, -1 on failure */
+int is_dll_empty(dll_t *list);						/* return 0 if empty, -1 if non-empty */
+void drain_dll(dll_T *list);						/* delete all nodes from dll, but do not free the data */
 #endif
